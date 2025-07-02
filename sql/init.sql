fce57870b14dafd1710efc9c228aafe2ae60a4a2
Shuan <jhforever0076@gmail.com>
2025-07-02 17:44:56 +0800

    更新join兩表

--- sql/init.sql
+++ sql/init.sql
@@ -12,9 +12,61 @@ CREATE TABLE IF NOT EXISTS weather (
     wind_direction INT,
     rainfall FLOAT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE KEY uniq_weather (station_id, date)
+    UNIQUE KEY uniq_weather (date, station_id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-DESC weather;
-SELECT count(*) FROM weather;
-DROP table weather;
+CREATE TABLE IF NOT EXISTS gpx (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE,
+    uploader VARCHAR(100),
+    total_distance_km FLOAT,
+    total_ascent_m FLOAT,
+    total_descent_m FLOAT,
+    total_duration TIME,
+    ascent_time TIME,
+    descent_time TIME,
+    ascent_speed_kmh FLOAT,
+    ascent_speed_vmph FLOAT,
+    descent_speed_kmh FLOAT,
+    descent_speed_vmph FLOAT,
+    avg_speed_kmh FLOAT,
+    avg_speed_vmph FLOAT,
+    max_elevation_m FLOAT,
+    min_elevation_m FLOAT  
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- join成 VIEW 當gpx/weather更新時 能夠自動更新 如果令建新表會是靜態表 要手動更新麻煩
+CREATE OR REPLACE VIEW hiking AS
+SELECT
+    gpx.date,
+    gpx.uploader,
+    gpx.total_distance_km,
+    gpx.total_ascent_m,
+    gpx.total_descent_m,
+    gpx.total_duration,
+    gpx.ascent_time,
+    gpx.descent_time,
+    gpx.ascent_speed_kmh,
+    gpx.ascent_speed_vmph,
+    gpx.descent_speed_kmh,
+    gpx.descent_speed_vmph,
+    gpx.avg_speed_kmh,
+    gpx.avg_speed_vmph,
+    gpx.max_elevation_m,
+    gpx.min_elevation_m,
+    weather.station_id,
+    weather.season,
+    weather.temperature,
+    weather.humidity,
+    weather.wind_speed,
+    weather.wind_direction,
+    weather.rainfall
+FROM gpx
+LEFT JOIN weather
+    ON gpx.date = weather.date
+ORDER BY gpx.date;
+   
+
+select * from hiking;
+select * from weather;
+select * from gpx;
--- src/data_process/to_sql.py
+++ src/data_process/to_sql.py
@@ -2,15 +2,89 @@ import pandas as pd
 from src.config import DATA_INTERIM, SQLALCHEMY_DATABASE_URI
 from sqlalchemy import create_engine
 
-df = pd.read_csv(DATA_INTERIM / 'weather' / 'weather.csv')
+def weather_to_sql():
+    df = pd.read_csv(DATA_INTERIM / 'weather' / 'weather.csv')
+    df = df.astype(object).where(pd.notnull(df), None) # NaN轉None mysql才看得懂
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    connection = engine.raw_connection()
 
-df.to_sql(
-    name='weather',        # 對應 SQL 中的table名
-    con=engine,
-    if_exists='append',    # 已存在table就append
-    index=False            # 不把 DataFrame index 當欄位寫入
-)
+    columns = df.columns.to_list() # 你要寫進 DB 的欄位
 
-print(f"成功寫入{len(df)}筆資料進資料庫！")
+    # 動態產生sql query的值 不寫死 改欄位就不用回來改
+    placeholders = ", ".join(["%s"] * len(columns))
+    columns_sql = ", ".join([f"`{col}`" for col in columns])    
+    exclude_cols = ['date', 'station_id', 'id'] # 不該更新的欄位    
+    update_columns = [col for col in columns if col not in exclude_cols] # 自動產生要更新的欄位
+
+    update_sql = ", ".join([f"`{col}`=VALUES(`{col}`)" for col in update_columns])
+
+    insert_sql = f"""
+    INSERT INTO weather ({columns_sql})
+    VALUES ({placeholders})
+    ON DUPLICATE KEY UPDATE {update_sql}
+    """
+
+    data = [tuple(row[col] for col in columns) for _, row in df.iterrows()]
+
+    with connection.cursor() as cursor:
+        cursor.executemany(insert_sql, data)
+
+    connection.commit()
+    connection.close()
+    print(f"成功 upsert {len(data)} 筆天氣資料！")
+
+
+def gpx_to_sql():
+    df = pd.read_csv(DATA_INTERIM / 'gpx' / '桃山gpx.csv')
+    df = df.rename(columns={
+    "日期": "date",
+    "上傳此GPX用戶": "uploader",
+    "總距離水平(km)": "total_distance_km",
+    "總爬升高度(m)": "total_ascent_m",
+    "總下降高度(m)": "total_descent_m",
+    "總花費時間(HH:MM:SS)": "total_duration",
+    "起始到折返點時間(HH:MM:SS)": "ascent_time",
+    "折返到結束時間(HH:MM:SS)": "descent_time",
+    "上山水平速度(km/h)": "ascent_speed_kmh",
+    "上山垂直速度(m/h)": "ascent_speed_vmph",
+    "下山水平速度(km/h)": "descent_speed_kmh",
+    "下山垂直速度(m/h)": "descent_speed_vmph",
+    "全程平均水平速度(km/h)": "avg_speed_kmh",
+    "全程平均垂直速度(m/h)": "avg_speed_vmph",
+    "最高海拔(m)": "max_elevation_m",
+    "最低海拔(m)": "min_elevation_m",
+    })
+    df = df.astype(object).where(pd.notnull(df), None)
+
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    connection = engine.raw_connection()
+
+    columns = df.columns.to_list() # 欄位
+
+    # 動態產生sql query的值 不寫死 改欄位就不用回來改
+    placeholders = ", ".join(["%s"] * len(columns))
+    columns_sql = ", ".join([f"`{col}`" for col in columns])    
+    exclude_cols = [] # 不該更新的欄位, 唯一鍵    
+    update_columns = [col for col in columns if col not in exclude_cols] # 自動產生要更新的欄位
+
+    update_sql = ", ".join([f"`{col}`=VALUES(`{col}`)" for col in update_columns])
+
+    insert_sql = f"""
+    INSERT INTO gpx ({columns_sql})
+    VALUES ({placeholders})
+    ON DUPLICATE KEY UPDATE {update_sql}
+    """
+
+    data = [tuple(row[col] for col in columns) for _, row in df.iterrows()]
+
+    with connection.cursor() as cursor:
+        cursor.executemany(insert_sql, data)
+
+    connection.commit()
+    connection.close()
+
+    print(f"成功 upsert {len(df)}筆登山資料進資料庫！")
+
+weather_to_sql()
+gpx_to_sql()
